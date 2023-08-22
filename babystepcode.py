d051c5a8de6f816a792d6e6201c1a9533ed1da6c
DOyouKnowMuffinMan <62110718+hrcgunn@users.noreply.github.com>
2023-08-22 15:48:12 -0500

    babystepcodeV2.py
    Corrections to white spaces allowed for update to syntax and correction of missing spaces in the sentence structure

--- babystepcode.py
+++ babystepcode.py
@@ -1,11 +1,11 @@
 # Write your code here :-)
->>>print ('Hello, big dreamer')
+>>>print('Hello, big dreamer')
 # This program says hello and asks for my name
 print('what is your name?') #asks for name
 myName = input()
-print('that is a great name' + myName)
+print('that is a great name ' + myName)
 print('the length of your name is:')
 print(len(myName))
 print ('what is your age, or as the Germans say, "wie alt sind sie"?') #asks for age
 myAge=input()
-print('You will be'+str(int(myAge)+5 + 'in 5 years. Wow!')
+print('You will be '+str(int(myAge)+5 + ' in 5 years. Wow!')
